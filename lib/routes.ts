583d899d16adb9622337149c14fbb8572b05fac9
Harpreet Singh <szinghh@gmail.com>
2024-06-19 17:11:53 -0600

    feat: next auth

---
+++ lib/routes.ts
@@ -0,0 +1,3 @@
+export const ROOT = '/api/auth/signin';
+export const PUBLIC_ROUTES = ['/'];
+export const DEFAULT_REDIRECT = '/protected';
--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,19 @@
-export { auth as middleware } from './lib/auth';
+import { auth } from './lib/auth';
+import { DEFAULT_REDIRECT, PUBLIC_ROUTES, ROOT } from './lib/routes';
+
+export default auth(req => {
+  const { nextUrl } = req;
+
+  const isAuthenticated = !!req.auth;
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+
+  if (isPublicRoute && isAuthenticated)
+    return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
+
+  if (!isAuthenticated && !isPublicRoute)
+    return Response.redirect(new URL(ROOT, nextUrl));
+});
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images|public).*)'],
+};
